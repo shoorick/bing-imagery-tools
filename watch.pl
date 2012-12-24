88fa0e3a3097528f7a9d08354533e41c3cdbdfe3
Alexander Sapozhnikov <shoorick@mail.ru>
2012-12-25 03:22:54 +0600

    Added Twitter Bootstrap style

--- watch.pl
+++ watch.pl
@@ -42,8 +42,8 @@ sub check_tile {
 
     $state
         = ( $res->headers->header('X-VE-Tile-Info') eq 'no-tile' )
-        ? 'no'
-        : 'yes';
+        ? 'error'
+        : 'success';
 #    X-VE-Tile-Info: no-tile
 
 
@@ -94,12 +94,14 @@ app->start;
 __DATA__
 
 @@ form.html.ep
-%= form_for 'search' => begin
-Search for
-%= text_field 'address'
-%= submit_button
+%= form_for 'search' => ( 'class' => 'navbar-form form-search pull-left' ) => begin
+    %= t div => ( 'class' => 'input-append' ) => begin
+        %= text_field 'address' => ( 'placeholder' => 'Search' ) => ( 'class' => 'search-query' )
+        %= t 'button' => ( 'type' => 'submit' ) => ( 'class' => 'btn' ) => begin
+            <i class="icon-search"></i>
+        % end
+    % end
 % end
-
 
 @@ index.html.ep
 % layout 'default';
@@ -112,50 +114,94 @@ Search for
 % my $map = $places->[0];
 % if ( $map ) {
 %    title 'Result';
-%=   t 'h1' => 'Result';
-<div id="map"></div>
-<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.4/leaflet.css" /><!--[if lte IE 8]>
-<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.4/leaflet.ie.css" /><![endif]--><script src="http://cdn.leafletjs.com/leaflet-0.4/leaflet.js"></script><script>
-var map = L.map('map').setView([<%= $map->{'lat'} %>, <%= $map->{'lon'} %>], 14);
-L.tileLayer('http://{s}.tile.cloudmade.com/894a23ab6e0944fa8097f1e803d062da/1155/256/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> Contributors, <a href="http://www.openstreetmap.org/copyright">ODbL 1.0</a>, Imagery &copy; <a href="http://cloudmade.com">CloudMade</a>', maxZoom: 18}).addTo(map);
-var marker = L.marker([<%= $map->{'lat'} %>, <%= $map->{'lon'} %>]).addTo(map);
-</script>
-<ul class="places">
+<div class="span6"><div id="map"></div></div>
+<div class="span6">
+    <form action="subscribe">
+        <table class="places table table-striped with-check">
+            <tr>
+                <th><input type="checkbox" id="title-checkbox" name="title-checkbox" /> </th>
+                <th>See and subscribe</th>
+            </tr>
 % my %seen_at_this_page = ();
 % for my $place ( @$places ) {
 %   my $tile = shift @$tiles;
-<li class="<%= $tile->{'class'} . ' class-' . $place->{'class'} . ' type-' . $place->{'type'} . ( exists $seen_at_this_page{ $tile->{'tile'} } ? ' already' : '' ) %>">
-<a href="#<%= $tile->{'tile'} %>" onclick="marker.setLatLng([<%= $place->{'lat'} %>, <%= $place->{'lon'} %>]);map.panTo([<%= $place->{'lat'} %>, <%= $place->{'lon'} %>])"><%= $place->{'display_name'} %></a>
-</li>
+<tr class="<%= $tile->{'class'} . ' class-' . $place->{'class'} . ' type-' . $place->{'type'} . ( exists $seen_at_this_page{ $tile->{'tile'} } ? ' already' : '' ) %>">
+<td><% unless ( $tile->{'class'} eq 'success' || exists $seen_at_this_page{ $tile->{'tile'} } ) { %><input type="checkbox" /><% } %></td>
+<td><a href="#<%= $tile->{'tile'} %>" onclick="marker.setLatLng([<%= $place->{'lat'} %>, <%= $place->{'lon'} %>]);map.panTo([<%= $place->{'lat'} %>, <%= $place->{'lon'} %>])"><%= $place->{'display_name'} %></a>
+</td>
 %   $seen_at_this_page{ $tile->{'tile'} } = 1;
 % }
-</ul>
+</table>
+<button type="submit" class="btn btn-primary">Subscribe</button>
+</form>
+</div>
 % }
 % else {
 %    title 'Not Found';
-%=   t 'h1' => 'Not Found';
 % }
 
-%= include 'form';
+
+    <script src="http://cdn.leafletjs.com/leaflet-0.4/leaflet.js"></script>
+    <script>
+        var map = L.map('map').setView([<%= $map->{'lat'} %>, <%= $map->{'lon'} %>], 14);
+        L.tileLayer('http://{s}.tile.cloudmade.com/894a23ab6e0944fa8097f1e803d062da/1155/256/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> Contributors, <a href="http://www.openstreetmap.org/copyright">ODbL 1.0</a>, Imagery &copy; <a href="http://cloudmade.com">CloudMade</a>', maxZoom: 18}).addTo(map);
+        var marker = L.marker([<%= $map->{'lat'} %>, <%= $map->{'lon'} %>]).addTo(map);
+
+        $(document).ready(function(){
+            $("th input:checkbox").click(function() {
+                var checkedStatus = this.checked;
+                var checkbox = $(this).parents('.places').find('tr td:first-child input:checkbox');
+                checkbox.each(function() {
+                    this.checked = checkedStatus;
+                    if (checkedStatus == this.checked) {
+                        $(this).closest('.checker > span').removeClass('checked');
+                    }
+                    if (this.checked) {
+                        $(this).closest('.checker > span').addClass('checked');
+                    }
+                });
+            });
+        });
+    </script>
+
 
 @@ layouts/default.html.ep
 <!DOCTYPE html>
 <html>
   <head><title><%= title %></title>
-  <style>
-  body { font-family: "PT Sans","Droid Sans","Liberation Sans",sans-serif; color: #000; background: #fff }
-  a:link { color: #006 }
-  a:visited { color: #303 }
-  .unknown { background: #eee }
-  .yes { background: #cfd }
-  .no  { background: #fba }
-  .places { margin-right: 50% }
-  .places li { margin-bottom: 0.1em; }
-  .places a { text-decoration: none }
-  #map { height: 400px; width:50%; float:right }
-  .already td, li.already { font-size: 75%; color: #665 }
-  </style>
+    <link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="/css/bootstrap-responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.4/leaflet.css" />
+    <!--[if lte IE 8]> <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.4/leaflet.ie.css" /><![endif]-->
+    <style>
+      body { font-family: "PT Sans","Droid Sans","Liberation Sans",sans-serif }
+      a:link { color: #006 }
+      a:visited { color: #303 }
+    /*  .table tbody tr.success td { background: #cfd }*/
+      .table tbody tr.error   td { background: #fba }
+      #map { height: 400px; /*width:50%; float:right*/ }
+      .already td, li.already { font-size: 75%; color: #665 }
+    </style>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
   </head>
-  <body><%= content %></body>
+  <body>
+      <div class="navbar navbar-static-top">
+          <div class="navbar-inner">
+        <a class="brand" href="/" title="bing imagery — analyze and subscribe">bias</a>
+
+%= include 'form';
+        <ul class="nav">
+
+          <li><a href="/subscription">Subscription</a></li>
+        </ul>
+      </div>
+    </div>
+      <div class="container-fluid">
+          <div class="row-fluid">
+              <%= content %>
+          </div>
+      </div>
+  </body>
 </html>
